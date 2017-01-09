ad90caf87d8e66c8794dcb940b92bddffcd1cd86
googry <sjjeong1225@gmail.com>
2017-01-09 16:34:43 +0900

    Subscribe 여러개 등록하면 어떤 현상이 일어나는지
    Bus가 등록된 곳에 모두 이벤트를 뿌리게 된다.

--- MyApplication/app/src/main/java/com/example/googry/myapplication/FragmentA.java
+++ MyApplication/app/src/main/java/com/example/googry/myapplication/FragmentA.java
@@ -10,73 +10,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.squareup.otto.Bus;
+import com.squareup.otto.Subscribe;
+
 /**
  * Created by Googry on 2017-01-02.
  */
 
 public class FragmentA extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
     public FragmentA() {
-    }
-
-    public static FragmentA newInstance(String param1, String param2) {
-        FragmentA fragment = new FragmentA();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Bundle args = getArguments();
-        if (args != null) {
-            mParam1 = args.getString(ARG_PARAM1);
-            mParam2 = args.getString(ARG_PARAM2);
-        }
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
-        Log.v("Googry", "Param1 : " + mParam1 + " Param2 : " + mParam2);
         return inflater.inflate(R.layout.fragment_a, container, false);
     }
 
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        BusProvider.getInstance().register(this);
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString() + " must implement OnfragmentInteractionListener");
-        }
+    public void onDestroy() {
+        BusProvider.getInstance().unregister(this);
+        super.onDestroy();
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    public interface OnFragmentInteractionListener {
-        void onFragmentInteraction(Uri uri);
+    @Subscribe
+    public void onEvent(Event event){
+        Log.i("googry","FragmentA "+ event.getStr());
     }
 }
